70a3f050022e3984acc8e1180acc8fa408515d30
Dark Milton <hegdesumanth8@gmail.com>
2023-06-03 02:34:18 +0530

    model working

--- client/classroom/src/main.js
+++ client/classroom/src/main.js
@@ -6,6 +6,7 @@ import gsap from 'gsap'
 //Canvas
 const canvas = document.querySelector('canvas');
 const main = document.querySelector('.select');
+const select = document.querySelector('.button-17');
 const formContainer = document.querySelector('.form-container');
 //Scene
 const scene = new THREE.Scene();
@@ -97,7 +98,7 @@ gltfLoader.load("./models/C/Classroom1.gltf", (gltf) => {
     }
     cameraMovement(-10.29, 1.966, -7.66);
     cameraRotation(-2.89, -0.91, -2.94);
-    window.addEventListener('mouseup', async() => {
+    select.addEventListener('click', async() => {
         // camera.position.x= 1.49;
         // camera.position.y= 2.04;
         // camera.rotation.y= 1;
@@ -124,7 +125,10 @@ gltfLoader.load("./models/C/Classroom1.gltf", (gltf) => {
                 main.style.display = "none"
                 // formContainer.style.display = "block"
                 // lookAtPosition(400, 400, 0)
-                position = 1;
+                setInterval(()=> {
+                    position = 1;
+                },1000)
+                // position = 1;
                 // console.log(camera.rotation)
                 // break;
                 // default: 
@@ -182,10 +186,41 @@ const onMouseMove = (event) => {
         intersects[i].object.material.color.set(0x000000)
     }
 }
+// window.addEventListener('click', (e) => {
+//     if(position==1)
+//         onMouseMove(e);
+// })
+
+
+
+
+
+
 window.addEventListener('click', (e) => {
-    if(position==1)
-        onMouseMove(e);
+    if(position != 1) return;
+    pointer.x = (e.x / window.innerWidth) * 2 - 1;
+    pointer.y = -(e.y / window.innerHeight) * 2 + 1;
+    raycaster.setFromCamera(pointer, camera); 
+    const intersects=raycaster.intersectObjects (scene.children);
+    console.log("Click here");
+    for (let i = 0; i < intersects.length; i++) { 
+        console.log("Click");
+        // console.log(intersects[i].object.material.name);
+        const str = intersects[i].object.material.name
+        if(str.includes('StingrayPBS5')) {
+            console.log(intersects[i])
+            // intersects[i].object.material.color.set(0x000000)
+            // intersects[i].object.material.color.r = 1
+            formContainer.style.display = "block"
+            break;
+        }
+    }
 })
+
+
+
+
+
 
 
 

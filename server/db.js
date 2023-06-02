d617bc7ea4b3080cad8550cceff6f34e5f3bd429
colonelkp <colonelkpunit3112001@gmail.com>
2023-06-03 02:53:39 +0530

    otpauthenticationcompleted

--- server/db.js
+++ server/db.js
@@ -1,11 +1,43 @@
- async function call(){
-const {firebase, applicationDefault,cert}=await require('firebase-admin/app');
-const {getFirestore,Timestamp, FieldValue, Filter}=require('firbase-admin/firestore');
-const serviceAccount=require("./creds.json");
-const { getStorage } = require('firebase-admin/storage');
-const webapp=initializeApp({
-  credential:cert(serviceAccount)
-})
-const db =getFirestore()
-module.exports ={db}
- }
+// // const {firebase, applicationDefault,cert}=await require('firebase-admin/app');
+// // const {getFirestore,Timestamp, FieldValue, Filter}=require('firbase-admin/firestore');
+// const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
+// const { getFirestore, Timestamp, FieldValue, Filter } = require('firebase-admin/firestore');
+
+// initializeApp({
+//   credential: applicationDefault()
+// });
+
+// const db = getFirestore();
+
+// // const serviceAccount=require("./creds.json");
+// // const { getStorage } = require('firebase-admin/storage');
+// // const webapp=initializeApp({
+// //   credential:cert(serviceAccount)
+// // })
+// // const db =getFirestore(webapp)
+// module.exports ={db}
+const { getDatabase } = require("firebase/database");
+
+// const database = getDatabase();
+
+
+
+const { initializeApp } = require("firebase/app");
+
+// TODO: Replace the following with your app's Firebase project configuration
+// See: https://firebase.google.com/docs/web/learn-more#config-object
+const firebaseConfig = {
+  // ...
+  // The value of `databaseURL` depends on the location of the database
+  // databaseURL: "https://DATABASE_NAME.firebaseio.com",
+  databaseURL: "https://htmlbackend-ad937-default-rtdb.firebaseio.com",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+// Initialize Realtime Database and get a reference to the service
+const database = getDatabase(app);
+
+module.exports = database;
--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 const express=require("express");
 const cors=require("cors");
-const {db}=require("./db.js");
-const expressAsyncHandler = require("express-async-Handler");
 const dotenv=require("dotenv");
-const otpg=require("./genrateotp.js");
-const nodemailer=require("nodemailer");
-const { sendmail } = require("./sendmail.js");
+const database = require('./db')
+
+const { ref, child, get, set  } = require("firebase/database");
+// const otpg=require("./genrateotp.js");
+// const accountSid = process.env.TWILIO_ACCOUNT_SID;
+// const authToken = process.env.TWILIO_AUTH_TOKEN;
+// const client = require('twilio')(accountSid, authToken);
 // const transporter = require("transporter");
 const webapp=express();
 webapp.use(express.json());
@@ -18,13 +20,157 @@ webapp.get('/', async(req, res) => {
     })
 })
 // webapp.post("/ref",async(req, res)=>{
-//     const email=req.body.email;
-//     console.log(email);
-//     const pass=req.body.password;
+//     const phno=req.body.phonenumber;
+//     // console.log(email);
+//     // const pass=req.body.password;
+//     module.exports={phno};
 //     const otp=otpg();
-//     res.send({msg:"the otp is "+otp})
+//     module.exports={otp};
+//     res.send({msg:"the otp is "+otp,phno})
+//     await fun();
 // })
-webapp.post("/ref", sendmail)
-webapp.listen(3000,()=>{
+const accountSid = "ACb8802d784f0812ccb8a0c78f4b82e930";
+const authToken = "63fdda249b88e66c987ca25730be61d4";
+const verifySid = "VA344034e4377f25a63c37c646e5330ea3";
+const client = require("twilio")(accountSid, authToken);
+// const doc = require("firestore-admin/doc");
+// const setDoc = require("./firebase-admin/set-doc")
+// import { doc, setDoc } from 'firebase/firestore';
+// constdoc=require("firebase-admin/firestore/doc")
+webapp.post("/verifyotp", async(req, res) => {
+//     client.verify.v2.services(verifySid)
+//     .verificationChecks
+//     .create({to: "+917349376269", code: "121312"})
+//     .then(verification_check => {
+//         console.log(verification_check.status)
+//         // if(verification_check.status == "approved") {
+//         //     res.status(200).send({success: true,
+//         //     message: approved});
+//         // } else {
+//         //     res.status(400).send({success: true,
+//         //     message: "Otp incorrect"
+//         // });
+//         // }
+//         res.status(200).send({success: true});
+//     })
+// .catch(err => {
+// res.status(600).send({success: false,
+// message: err.message});
+// })
+
+
+
+
+
+
+
+    const phno= req.body.phonenumber
+    const otpCode= req.body.otpcode
+    const dbRef = ref(database);
+    get(child(dbRef, `users/${phno}`)).then((snapshot) => {
+    if (snapshot.exists()) {
+        console.log(snapshot.val());
+        // sid =snapshot.val().sid
+    } else {
+        console.log("No data available");
+    }
+    })
+    .then(()=> {
+    // console.log(sid);
+
+    client.verify.v2.services(verifySid)
+    .verificationChecks
+    .create({to: phno, code: otpCode})
+    .then(verification_check => {
+        console.log(verification_check.status)
+        if(verification_check.status == "approved") {
+            res.status(200).send({success: true,
+            message: "approved"});
+        } else {
+            res.status(400).send({success: true,
+            message: "Otp incorrect"
+        });
+        }
+        // res.status(200).send({success: true});
+    })
+.catch(err => {
+res.status(600).send({success: false,
+message: err.message});
+})
+    }).catch((error) => {
+    console.error(error);
+    res.status(500).send({success: false})
+    });
+})
+webapp.post("/authenticate",async(req,res)=>{
+
+    // const otp=otpg();
+    const seatId = req.body.seatId;
+    const phno=req.body.phonenumber;
+    const name=req.body.name;
+    const startTime=req.body.startTime;
+    //const docRef = db.collection('people').doc('associates');
+    // const userRef = db.doc("associates");
+    // await setDoc(userRef, {
+    //     name: name,
+    //     phno: phno
+    // });
+    // const res1= await db.collection('cities').add({
+    //     name: 'Tokyo',
+    //     phno: 'Japan'
+    //   });
+
+    //   console.log('Added document with ID: ', res.id);
+
+// await docRef.set({
+//   name: name,
+//   phno: phno,
+// });
+    client.verify.v2
+    .services(verifySid)
+    .verifications.create({ to: phno, channel: "sms" })
+    .then(message => {
+        try{const sid = message.sid
+        console.log(sid);
+        set(ref(database, 'users/'+phno), {
+            name: name,
+            phno: phno,
+            seatId: seatId,
+            sid: sid,
+            startTime: startTime
+          })
+          .then(() => {
+                res.status(200).send({
+                success: true,
+                });
+          });}
+    catch(err){
+        res.status(500).send({success: false, message: err.message});
+    }
+
+    })
+
+
+    // readline.question("Please enter the OTP:", (otpCode) => {
+    //     client.verify.v2
+    //       .services(verifySid)
+    //       .verificationChecks.create({ to: "+918494912037", code: otpCode })
+    //       .then((verification_check) => console.log(verification_check.status))
+    //       .then(() => readline.close());
+    //   });
+
+
+// .then()
+
+
+
+//
+
+})
+webapp.listen(3100,()=>{
     console.log("running successfully")
 })
+
+
+
+

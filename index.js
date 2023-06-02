983c79006e1de7643b8eee324ba935653031e5c6
colonelkp <colonelkpunit3112001@gmail.com>
2023-06-03 04:20:41 +0530

    Server completed

--- index.js
+++ index.js
@@ -4,11 +4,6 @@ const dotenv=require("dotenv");
 const database = require('./db')
 
 const { ref, child, get, set  } = require("firebase/database");
-// const otpg=require("./genrateotp.js");
-// const process.env.ACCOUNTSID = process.env.TWILIO_ACCOUNT_SID;
-// const process.env.AUTHTOKEN = "63fdda249b88e66c987ca25730be61d4"; = process.env.TWILIO_AUTH_TOKEN;
-// const client = require('twilio')(process.env.ACCOUNTSID, process.env.AUTHTOKEN = "63fdda249b88e66c987ca25730be61d4";);
-// const transporter = require("transporter");
 const webapp=express();
 webapp.use(express.json());
 webapp.use(cors());
@@ -23,10 +18,6 @@ const accountsid = process.env.TWILIO_ACCOUNT_SID
 const authToken = process.env.TWILIO_AUTH_TOKEN
 const verifySid = process.env.VERIFYSID
 const client = require("twilio")(accountsid, authToken);
-// const doc = require("firestore-admin/doc");
-// const setDoc = require("./firebase-admin/set-doc")
-// import { doc, setDoc } from 'firebase/firestore';
-// constdoc=require("firebase-admin/firestore/doc")
 webapp.post("/verifyotp", async(req, res) => {
 
 
@@ -36,14 +27,11 @@ webapp.post("/verifyotp", async(req, res) => {
     get(child(dbRef, `users/${phno}`)).then((snapshot) => {
     if (snapshot.exists()) {
         console.log(snapshot.val());
-        // sid =snapshot.val().sid
     } else {
         console.log("No data available");
     }
     })
     .then(()=> {
-    // console.log(sid);
-
     client.verify.v2.services(verifySid)
     .verificationChecks
     .create({to: phno, code: otpCode})
@@ -57,7 +45,6 @@ webapp.post("/verifyotp", async(req, res) => {
             message: "Otp incorrect"
         });
         }
-        // res.status(200).send({success: true});
     })
 .catch(err => {
 res.status(600).send({success: false,
@@ -69,29 +56,10 @@ message: err.message});
     });
 })
 webapp.post("/authenticate",async(req,res)=>{
-
-    // const otp=otpg();
     const seatId = req.body.seatId;
     const phno=req.body.phonenumber;
     const name=req.body.name;
     const startTime=req.body.startTime;
-    //const docRef = db.collection('people').doc('associates');
-    // const userRef = db.doc("associates");
-    // await setDoc(userRef, {
-    //     name: name,
-    //     phno: phno
-    // });
-    // const res1= await db.collection('cities').add({
-    //     name: 'Tokyo',
-    //     phno: 'Japan'
-    //   });
-
-    //   console.log('Added document with ID: ', res.id);
-
-// await docRef.set({
-//   name: name,
-//   phno: phno,
-// });
     client.verify.v2
     .services(verifySid)
     .verifications.create({ to: phno, channel: "sms" })
@@ -113,25 +81,7 @@ webapp.post("/authenticate",async(req,res)=>{
     catch(err){
         res.status(500).send({success: false, message: err.message});
     }
-
     })
-
-
-    // readline.question("Please enter the OTP:", (otpCode) => {
-    //     client.verify.v2
-    //       .services(verifySid)
-    //       .verificationChecks.create({ to: "+918494912037", code: otpCode })
-    //       .then((verification_check) => console.log(verification_check.status))
-    //       .then(() => readline.close());
-    //   });
-
-
-// .then()
-
-
-
-//
-
 })
 const port =process.env.PORT || 3100
 webapp.listen(port,()=>{
